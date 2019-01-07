80b753750bb0bf594057e1024f5479679cbf401a
JimmyAgudo <44040646+JimmyAgudo@users.noreply.github.com>
2019-01-07 17:34:59 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,38 @@
+$("#A").dblclick(function() {
+$("#crab").show();
+$("#x").show();
+$("#p").show();
+});
+
+$("#p").click(function() {
+$("#en").show();
+});
+
+$("#en").hover(function(){
+    $("#k").show();
+    });
+    
+    $("#k").click(function(){
+    $("#m").show();
+    });
+    $("#m").hover(function(){
+    $("#m").hide();
+     $("#b").show();
+    $("#en").hide();
+    $("#p").hide();
+    $("#x").hide();
+    $("#crab").hide();
+    $("#A").hide();
+    $("#k").hide();
+    $("#b").hide();
+    $("#F").hide();
+    $("#L").show();
+    $("#N").show();
+    
+    });
+    
+    
+    
+    
+   
+  
